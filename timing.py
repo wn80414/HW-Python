d3b27f76372a92362a43047606af19e2d4d7aa3e
William Nguyen <william.g.nguyen@sjsu.edu>
2021-10-14 04:36:23 -0700

    Bug Fix timing.py

--- timing.py
+++ timing.py
@@ -1,6 +1,4 @@
 import time
-
-
 def calculate_time(func):
     """
     Decorator that calculates the run time of a function.
@@ -16,11 +14,14 @@ def calculate_time(func):
     >>> calculate_time(time.sleep(2))
     Total time 1.1920928955078125e-06
     """
-    current_time = time.time()
-    func()
-    after_time = time.time()
-    run_time = after_time - current_time  #Finds the difference between current_time and after_time to find total run time
-    print("Total time", run_time)
+    def wrapper():
+        currentTime = time.time()
+        funcValue = func()                 
+        afterTime = time.time()
+        runTime = afterTime - currentTime  #Finds the difference between currentTime and afterTime to find total run time
+        print("Total time", runTime)
+        return value
+    return wrapper
 
 
 
